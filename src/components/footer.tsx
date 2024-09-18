3e1084f07354f5021a1425951cf0064c0d3a7f3a
Andranik Virabyan <gmparstone99@gmail.com>
2024-09-18 01:20:26 -0800

    hhh

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <footer className="mt-auto text-center text-zinc-400 py-5 px-7 border-t">
-        <small>&copy; 2040. All rights reserved.</small>
+        <small>&copy; 2041. All rights reserved.</small>
     </footer>
   )
 }
